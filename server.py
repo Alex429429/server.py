5f50bac47154104187680bdcc5c469e8495e3dbe
Alex429429 <sasha.ignatenko07@gmail.com>
2025-06-04 16:16:39 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify
 import time
+import os
 
 app = Flask(__name__)
 
 @app.route('/ping')
 def ping():
-    server_time = int(time.time() * 1000)  # время в миллисекундах
+    server_time = int(time.time() * 1000)  # миллисекунды
     return jsonify({'serverTime': server_time})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
